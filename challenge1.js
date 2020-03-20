a38027fb3b8a3d813af74a92b34e9141e2f5b27d
Kalea <kaleapalmer@nyu.edu>
2020-03-20 15:03:45 -0700

    Located signup buttons, going to assign click handlers next

--- challenge1.js
+++ challenge1.js
@@ -4,12 +4,32 @@ const chrome = require('selenium-webdriver/chrome');
 async function getProductInfo(url) {
     //object that will hold the price and currency of the product
     const itemObj = {}
+
     let driver = await new Builder().forBrowser('chrome').build();
     try {
         await driver.get(url);
+
+        //Find the ele that holds the currency
+        //TODO Find the correct element that holds the currency
+        let obj = await driver.findElement(By.xpath("//*[@id=\"viewport\"]/div[4]"));
+
+        // console.log(obj)
+        // let objparse = await obj.getText();
+        // console.log(objparse)
+
+
+        //Wait for element that holds the price to load and
         let priceElement= await driver.wait(until.elementLocated(By.css("div[data-test='product-price']")), 30000, 'Timed out after 30 seconds', 5000);
-        let price = await priceElement.getText();
-        console.log(price)
+        itemObj.price = await priceElement.getText();
+
+        //TODO parse price from string to int before putting in the object
+        // itemObj.price = price.parseInt();
+        console.log(itemObj)
+
+
+
+
+        // console.log(itemObj)
     } finally {
         await driver.quit();
     }
--- challenge2.js
+++ challenge2.js
@@ -1,19 +1,32 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
-function challenge2(url) {
-    $.get(url , function( data ) {
-        $( ".result" ).html( data );
-        alert( "Load was performed." );
-    });
-    console.log("challenge 2 is finished")
+async function assignClickHandler() {
+    //url that we are assigning a click handler to
+    const url = "https://curiositystream.com/";
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get(url);
+
+        let priceElement= await driver.wait(until.elementsLocated(By.css("a[href='/signup']")), 30000, 'Timed out after 30 seconds', 5000);
+
+        // console.log(await priceElement.getText());
+        for(let e of priceElement) {
+                console.log(await e.getText());
+            }
+
+        // console.log(itemObj)
+    } finally {
+        // await driver.quit();
+    }
 }
 
 /**
- * Will call other functions to perform challenge 1
+ * Will call other functions to perform challenge 2
  */
 function execute() {
-    //change this for different target URLS
-    const url = "https://curiositystream.com/"
-    challenge2(url);
+    assignClickHandler();
 }
 
 //call up each challenge
